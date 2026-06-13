68b3dcca4b5d94f22fe3a2352bed5c369591953c
NPE Tech Admin <contact@npe-techs.com>
2026-06-13 06:23:30 +0100

    Fix: route all traffic through WSGI for WhiteNoise static serving, use CompressedStaticFilesStorage

--- recommender_project/settings.py
+++ recommender_project/settings.py
@@ -64,7 +64,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True
